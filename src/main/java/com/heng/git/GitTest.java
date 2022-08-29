2ca2aa2a4cde3e3c3d387e5b8c93f67b1ed064f1
Hengkejun <2836842223@qq.com>
2022-08-29 16:23:28 +0800

    first commit

---
+++ src/main/java/com/heng/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.heng.git;
+
+/**
+ * @author hengkejun
+ * @create 2022/8/29
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git idea");
+    }
+}
