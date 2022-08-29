6e90bb39b434f49cffaae3a0246d2e213bd8f889
Hengkejun <2836842223@qq.com>
2022-08-29 19:51:02 +0800

    push commit

--- src/main/java/com/heng/git/GitTest.java
+++ src/main/java/com/heng/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("master commit1");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
