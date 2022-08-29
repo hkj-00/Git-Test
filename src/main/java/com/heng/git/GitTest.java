852f24ddbd25d8eb6fb0d89b2a8a6007cf991e04
hengkejun <74610670+hkj-00@users.noreply.github.com>
2022-08-29 20:11:01 +0800

    pull commit

--- src/main/java/com/heng/git/GitTest.java
+++ src/main/java/com/heng/git/GitTest.java
@@ -14,6 +14,5 @@ public class GitTest {
         System.out.println("master commit1");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
-        System.out.println("push test!");
-    }
+        System.out.println("pull test!");
 }
